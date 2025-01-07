e27448a6126810b0d57924bf388549e56ce3af4f
OwinoMichael <owinomichael1400@gmail.com>
2025-01-07 00:19:03 -0500

    First Commit

---
+++ src/main/java/com/mike/mike/BankingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mike.mike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mike/mike/BankingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.mike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
