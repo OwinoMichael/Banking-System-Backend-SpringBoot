12d618b16775cfa91d0ed6797d8785f3959ccb7b
OwinoMichael <owinomichael1400@gmail.com>
2025-02-16 23:59:01 -0500

    Account entity

--- src/main/java/com/mike/mike/Account/Account.java
+++ src/main/java/com/mike/mike/Account/Account.java
@@ -1,4 +1,14 @@
 package com.mike.mike.Account;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "account")
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer account_id;
+
+    private String accountNumber;
 }
--- src/main/java/com/mike/mike/Account/AccountRepository.java
+++ src/main/java/com/mike/mike/Account/AccountRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-public interface AccountRepository {
+public interface AccountRepository extends JpaRepository<Integer, Account>{
 }
